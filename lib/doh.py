3bad1075e46b97fc61b1b2570b62eebeccd1d2ee
Matias Fernandez <matias.fernandez@gmail.com>
2022-03-06 22:20:18 -0300

    Added code for handling initial DNS requests to the DoH Servers, using "initial_dns" config directive.

--- lib/doh.py
+++ lib/doh.py
@@ -1,13 +1,35 @@
-from turtle import write_docstringdict
 import requests
 import random
 import dnslib
 import re
 from box import Box
 from lib import globals
+from urllib3.util import connection
+import dns.resolver
 
 import requests_cache
 requests_cache.install_cache('cache', backend='memory')
+
+def custom_dns_resolver(hostname, type='A'):
+    """Resolve the hostname to an IP address"""
+    nameservers = globals.config.service.initial_dns
+    custom_resolver = dns.resolver.Resolver()
+    custom_resolver.nameservers = nameservers
+    answer = custom_resolver.query(hostname, type)
+
+    return str(random.choice(answer))
+
+_orig_create_connection = connection.create_connection
+def patched_create_connection(address, *args, **kwargs):
+    """Wrap urllib3's create_connection to resolve the name elsewhere"""
+    # resolve hostname to an ip address; use your own
+    # resolver here, as otherwise the system resolver will be used.
+    host, port = address
+    hostname = custom_dns_resolver(host)
+
+    return _orig_create_connection((hostname, port), *args, **kwargs)
+connection.create_connection = patched_create_connection
+
 
 class DOH:
     """ main class to handle all our DoH requests """
@@ -58,14 +80,15 @@ class DOH:
 
         dnsdata = dnslib.DNSRecord.parse(wireframe)
         dnsdomain = dnsdata.q.get_qname()
+        qtype = dnslib.QTYPE.get(k=dnsdata.q.qtype)
+
+        print(f"Handling query: ({qtype}) {dnsdomain}")
 
         retval = None
         domconfig = self.get_domain_config(globals.config.default, wireframe)
 
         if 'static' in domconfig:
             # handle "static" domain configuration
-
-            qtype = dnslib.QTYPE.get(k=dnsdata.q.qtype)
 
             if qtype in domconfig.static:
                 # reply for static configured domain match
